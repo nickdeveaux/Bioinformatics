48bd56b08dd8ef13fb3f60b227d33dfdcdca5b62
nrdeveaux@gmail.com <nrdeveaux@gmail.com>
2013-11-13 22:36:06 -0500

    Implemented clump finding algorithm

--- kmerclump.py
+++ kmerclump.py
@@ -14,21 +14,49 @@ import sys
 import os
 import utils
 import fileinput
+from sets import Set
 
 def main():
     args = []
     for line in fileinput.input():
-        args.append(line.rstrip())
+        elems = line.rstrip().split(' ')
+        for el in elems:
+            if el != 'Input':
+                args.append(el)
     input = args[0]
-    k = args[1]
-    L = args[2]
-    t = args[3]
+    k = int(args[1])
+    L = int(args[2])
+    t = int(args[3])
     kmers = find_clumps(input, k, L, t);
     utils.print_array(kmers)
-    
-def find_clumps(input, k, L, t):
-    importpdb;
 
+# create a dictionary of all locations of all kmers in input
+def find_clumps(input, k, L, t):
+    i = 0
+    kmer_locations = {}
+    kmer_clumps = Set([])
+    while ( i + k <= len(input)):
+        kmer = input[i:i+k] 
+        if kmer_locations.has_key(kmer):
+            kmer_locations[kmer].append(i)
+            if is_clump(kmer_locations[kmer], L, t, i):
+                kmer_clumps.add(kmer)
+        else:
+            kmer_locations[kmer]=[i]
+        i += 1
+    return kmer_clumps
+
+# Are there t or more distinct kmers within distance L of i?
+def is_clump(kmer_indices, L, t, i):
+    count = 0
+    for index in kmer_indices:
+        if i - index <= L:
+            count += 1
+    if count >= t:
+        return True
+    else:
+        return False
+            
 
 if __name__ == '__main__':
 	main()
