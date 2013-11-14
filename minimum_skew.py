151bc0c1c8c87ad92cd4d55580f587f316b7796c
nrdeveaux@gmail.com <nrdeveaux@gmail.com>
2013-11-14 09:05:31 -0500

    corrections to have multiple indices for a given ratio

--- minimum_skew.py
+++ minimum_skew.py
@@ -4,6 +4,10 @@
 minimum_skew.py
 
 Created by Nick DeVeaux on 2013-11-14.
+
+Minimum Skew Problem: Find a position in a genome minimizing the skew.
+     Input: A DNA string Genome.
+     Output: All integer(s) i minimizing Skew(Prefixi (Text)) among all values of i (from 0 to |Genome|).
 """
 
 import sys
@@ -17,16 +21,19 @@ def main():
         args.append(line.rstrip())
     input = args[0]
     skew_array = iterative_skew(input)
-    import pdb; pdb.set_trace()
     utils.print_array(skew_array)
 
 def iterative_skew(input):
     base_counts = {'C':0, 'G':0, 'A':0, 'T':0}
-    ratio = {}
-    i = 0
+    ratio = {0:[0]}
+    i = 1
     for char in input:
         base_counts[char] += 1
-        ratio[base_counts['G']-base_counts['C']] = i
+        diff = base_counts['G']-base_counts['C']
+        if ratio.has_key(diff):
+            ratio[diff].append(i)
+        else:
+            ratio[diff] = [i]
         i += 1
     return ratio[min(ratio)]
 
