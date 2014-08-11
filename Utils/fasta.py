76c9b6251431702ee9b6c57b6ee53c24450523da
Nick De Veaux <nrdeveaux@gmail.com>
2014-08-11 18:53:58 +0200

    fasta parsing

---
+++ Utils/fasta.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+
+Created by Nick DeVeaux on 2013-11-13.
+"""
+
+def fasta():
+
+def parse(lines):
+	fastas = []
+	for line in lines:
+		if len(line) > 0:
+			if line[0] == '>':
+				f = fasta()
+				f.identifier = line[1:]
+				f.content = ''
+				fastas.append(f)
+			else:
+				f.content += line
+	return fastas
+
