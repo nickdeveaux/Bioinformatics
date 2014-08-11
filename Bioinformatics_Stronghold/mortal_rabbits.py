1defe35056332fce896b55b9ed979eaaa429bdf5
Nick De Veaux <nrdeveaux@gmail.com>
2014-08-11 21:34:21 +0200

    mortal rabbits implemented by storing rabbits born at time t, and all rabbits at time t

--- Bioinformatics_Stronghold/mortal_rabbits.py
+++ Bioinformatics_Stronghold/mortal_rabbits.py
@@ -17,18 +17,26 @@ Assumption: A pair of rabbits only produce a new pair of offspring every month
 import utils
 import fileinput
 
+# dynamic programming approach: store all the values at time t
 def mortal_rabbits(n, m):
 	rabbits = []
-	rabbits[0] = 0
-	rabbits[1] = 1
+	born_rabbits = []
+	rabbits.insert(0, 0)
+	rabbits.insert(1, 1)
+	born_rabbits.insert(0, 0) # need to store index 0
+	born_rabbits.insert(1, 1) # need to store index 1
 	t = 1
 	while (t < n):
-		i = rabbits[t-1] + rabbits[t-2]
-		if t-m > 0:
-			i -= rabbits[t-m]
-		rabbits[t] = i
 		t += 1
-	return rabbits[t]
+		dead = 0
+		if t-m > 0:
+			dead = born_rabbits[t-m] + born_rabbits[t-m - 1]
+		i = rabbits[t-1] + rabbits[t-2] - dead
+
+		# born (at time t) = diff from the last time, plus the number of dead
+		born_rabbits.insert(t, i - rabbits[t-1] + dead)
+		rabbits.insert(t, i)
+	return rabbits[n]
 	
 
 def main():
